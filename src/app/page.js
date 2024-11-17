79f39bd30bb8113852834db3e854a18753032861
paullarasales <ahmadzufukli@gmail.com>
2024-11-17 17:17:55 +0800

    removed some contents

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="max-w-4xl mx-auto px-4">
-        <ProtectedRoute>
-          <RantJournal />
-        </ProtectedRoute>
+        
       </div>
     </div>
   );
